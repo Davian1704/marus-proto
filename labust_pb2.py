eee53d1099d63d72439add0119d227ba2ae3d7bf
Automation <generator@github.action>
2021-09-23 09:30:57 +0000

    Python code generated based on revision 9673260.

--- labust_pb2.py
+++ labust_pb2.py
@@ -22,7 +22,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='labust',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x0clabust.proto\x12\x06labust\x1a\tstd.proto\x1a\x10geographic.proto\x1a\x0egeometry.proto\"\x8d\x02\n\nFSPathInfo\x12\x1b\n\x06header\x18\x01 \x01(\x0b\x32\x0b.std.Header\x12\x0e\n\x06\x64\x65ltaR\x18\x02 \x01(\x01\x12\t\n\x01k\x18\x03 \x01(\x01\x12\x0b\n\x03xiR\x18\x04 \x01(\x01\x12\x0c\n\x04\x64xiR\x18\x05 \x01(\x01\x12\n\n\x02pi\x18\x06 \x01(\x01\x12\x0f\n\x07piTilda\x18\x07 \x01(\x01\x12\x1e\n\x03\x64rP\x18\x08 \x01(\x0b\x32\x11.geometry.Vector3\x12#\n\x08position\x18\t \x01(\x0b\x32\x11.geometry.Vector3\x12&\n\x0borientation\x18\n \x01(\x0b\x32\x11.geometry.Vector3\x12\x11\n\tcurvature\x18\x0b \x01(\x01\x12\x0f\n\x07torsion\x18\x0c \x01(\x01\"\xe8\x01\n\x17PointerPrimitiveService\x12\x0e\n\x06radius\x18\x01 \x01(\x01\x12\x17\n\x0fvertical_offset\x18\x02 \x01(\x01\x12)\n\x0eguidanceTarget\x18\x03 \x01(\x0b\x32\x11.geometry.Vector3\x12\x16\n\x0eguidanceEnable\x18\x04 \x01(\x08\x12\x16\n\x0ewrappingEnable\x18\x05 \x01(\x08\x12\x1d\n\x15streamlineOrientation\x18\x06 \x01(\x08\x12\x15\n\rguidanceTopic\x18\x07 \x01(\t\x12\x13\n\x0bradiusTopic\x18\x08 \x01(\tb\x06proto3')
+  serialized_pb=_b('\n\x0clabust.proto\x12\x06labust\x1a\tstd.proto\x1a\x10geographic.proto\x1a\x0egeometry.proto\"\x8d\x02\n\nFSPathInfo\x12\x1b\n\x06header\x18\x01 \x01(\x0b\x32\x0b.std.Header\x12\x0e\n\x06\x64\x65ltaR\x18\x02 \x01(\x01\x12\t\n\x01k\x18\x03 \x01(\x01\x12\x0b\n\x03xiR\x18\x04 \x01(\x01\x12\x0c\n\x04\x64xiR\x18\x05 \x01(\x01\x12\n\n\x02pi\x18\x06 \x01(\x01\x12\x0f\n\x07piTilda\x18\x07 \x01(\x01\x12\x1e\n\x03\x64rP\x18\x08 \x01(\x0b\x32\x11.geometry.Vector3\x12#\n\x08position\x18\t \x01(\x0b\x32\x11.geometry.Vector3\x12&\n\x0borientation\x18\n \x01(\x0b\x32\x11.geometry.Vector3\x12\x11\n\tcurvature\x18\x0b \x01(\x01\x12\x0f\n\x07torsion\x18\x0c \x01(\x01\"\xfd\x01\n\x17PointerPrimitiveService\x12\x0e\n\x06radius\x18\x01 \x01(\x01\x12\x17\n\x0fvertical_offset\x18\x02 \x01(\x01\x12)\n\x0eguidanceTarget\x18\x03 \x01(\x0b\x32\x11.geometry.Vector3\x12\x16\n\x0eguidanceEnable\x18\x04 \x01(\x08\x12\x16\n\x0ewrappingEnable\x18\x05 \x01(\x08\x12\x13\n\x0b\x66ovGuidance\x18\x06 \x01(\x08\x12\x1d\n\x15streamlineOrientation\x18\x07 \x01(\x08\x12\x15\n\rguidanceTopic\x18\x08 \x01(\t\x12\x13\n\x0bradiusTopic\x18\t \x01(\tb\x06proto3')
   ,
   dependencies=[std__pb2.DESCRIPTOR,geographic__pb2.DESCRIPTOR,geometry__pb2.DESCRIPTOR,])
 
@@ -180,22 +180,29 @@ _POINTERPRIMITIVESERVICE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='streamlineOrientation', full_name='labust.PointerPrimitiveService.streamlineOrientation', index=5,
+      name='fovGuidance', full_name='labust.PointerPrimitiveService.fovGuidance', index=5,
       number=6, type=8, cpp_type=7, label=1,
       has_default_value=False, default_value=False,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='guidanceTopic', full_name='labust.PointerPrimitiveService.guidanceTopic', index=6,
-      number=7, type=9, cpp_type=9, label=1,
+      name='streamlineOrientation', full_name='labust.PointerPrimitiveService.streamlineOrientation', index=6,
+      number=7, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='guidanceTopic', full_name='labust.PointerPrimitiveService.guidanceTopic', index=7,
+      number=8, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='radiusTopic', full_name='labust.PointerPrimitiveService.radiusTopic', index=7,
-      number=8, type=9, cpp_type=9, label=1,
+      name='radiusTopic', full_name='labust.PointerPrimitiveService.radiusTopic', index=8,
+      number=9, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -213,7 +220,7 @@ _POINTERPRIMITIVESERVICE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=342,
-  serialized_end=574,
+  serialized_end=595,
 )
 
 _FSPATHINFO.fields_by_name['header'].message_type = std__pb2._HEADER
