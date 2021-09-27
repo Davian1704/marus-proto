4cd96627edca10a8786fde60b53b7b4c05219f06
Automation <generator@github.action>
2021-09-27 17:21:21 +0000

    Python code generated based on revision 411b8ca.

--- auv_pb2.py
+++ auv_pb2.py
@@ -22,7 +22,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='auv',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\tauv.proto\x12\x03\x61uv\x1a\tstd.proto\x1a\x10geographic.proto\x1a\x0egeometry.proto\"1\n\x03NED\x12\r\n\x05north\x18\x01 \x01(\x01\x12\x0c\n\x04\x65\x61st\x18\x02 \x01(\x01\x12\r\n\x05\x64\x65pth\x18\x03 \x01(\x01\"\xa0\x03\n\x10NavigationStatus\x12\x1b\n\x06header\x18\x01 \x01(\x0b\x32\x0b.std.Header\x12,\n\x0eglobalPosition\x18\x02 \x01(\x0b\x32\x14.geographic.GeoPoint\x12$\n\x06origin\x18\x03 \x01(\x0b\x32\x14.geographic.GeoPoint\x12\x1a\n\x08position\x18\x04 \x01(\x0b\x32\x08.auv.NED\x12\'\n\x0c\x62odyVelocity\x18\x05 \x01(\x0b\x32\x11.geometry.Vector3\x12+\n\x10seafloorVelocity\x18\x06 \x01(\x0b\x32\x11.geometry.Vector3\x12&\n\x0borientation\x18\x07 \x01(\x0b\x32\x11.geometry.Vector3\x12+\n\x10orientation_rate\x18\x08 \x01(\x0b\x32\x11.geometry.Vector3\x12#\n\x11position_variance\x18\t \x01(\x0b\x32\x08.auv.NED\x12/\n\x14orientation_variance\x18\n \x01(\x0b\x32\x11.geometry.Vector3b\x06proto3')
+  serialized_pb=_b('\n\tauv.proto\x12\x03\x61uv\x1a\tstd.proto\x1a\x10geographic.proto\x1a\x0egeometry.proto\"1\n\x03NED\x12\r\n\x05north\x18\x01 \x01(\x01\x12\x0c\n\x04\x65\x61st\x18\x02 \x01(\x01\x12\r\n\x05\x64\x65pth\x18\x03 \x01(\x01\"\x9d\x03\n\x10NavigationStatus\x12\x1b\n\x06header\x18\x01 \x01(\x0b\x32\x0b.std.Header\x12,\n\x0eglobalPosition\x18\x02 \x01(\x0b\x32\x14.geographic.GeoPoint\x12$\n\x06origin\x18\x03 \x01(\x0b\x32\x14.geographic.GeoPoint\x12\x1a\n\x08position\x18\x04 \x01(\x0b\x32\x08.auv.NED\x12\'\n\x0c\x62odyVelocity\x18\x05 \x01(\x0b\x32\x11.geometry.Vector3\x12+\n\x10seafloorVelocity\x18\x06 \x01(\x0b\x32\x11.geometry.Vector3\x12&\n\x0borientation\x18\x07 \x01(\x0b\x32\x11.geometry.Vector3\x12*\n\x0forientationRate\x18\x08 \x01(\x0b\x32\x11.geometry.Vector3\x12\"\n\x10positionVariance\x18\t \x01(\x0b\x32\x08.auv.NED\x12.\n\x13orientationVariance\x18\n \x01(\x0b\x32\x11.geometry.Vector3b\x06proto3')
   ,
   dependencies=[std__pb2.DESCRIPTOR,geographic__pb2.DESCRIPTOR,geometry__pb2.DESCRIPTOR,])
 
@@ -131,21 +131,21 @@ _NAVIGATIONSTATUS = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='orientation_rate', full_name='auv.NavigationStatus.orientation_rate', index=7,
+      name='orientationRate', full_name='auv.NavigationStatus.orientationRate', index=7,
       number=8, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='position_variance', full_name='auv.NavigationStatus.position_variance', index=8,
+      name='positionVariance', full_name='auv.NavigationStatus.positionVariance', index=8,
       number=9, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='orientation_variance', full_name='auv.NavigationStatus.orientation_variance', index=9,
+      name='orientationVariance', full_name='auv.NavigationStatus.orientationVariance', index=9,
       number=10, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
@@ -164,7 +164,7 @@ _NAVIGATIONSTATUS = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=115,
-  serialized_end=531,
+  serialized_end=528,
 )
 
 _NAVIGATIONSTATUS.fields_by_name['header'].message_type = std__pb2._HEADER
@@ -174,9 +174,9 @@ _NAVIGATIONSTATUS.fields_by_name['position'].message_type = _NED
 _NAVIGATIONSTATUS.fields_by_name['bodyVelocity'].message_type = geometry__pb2._VECTOR3
 _NAVIGATIONSTATUS.fields_by_name['seafloorVelocity'].message_type = geometry__pb2._VECTOR3
 _NAVIGATIONSTATUS.fields_by_name['orientation'].message_type = geometry__pb2._VECTOR3
-_NAVIGATIONSTATUS.fields_by_name['orientation_rate'].message_type = geometry__pb2._VECTOR3
-_NAVIGATIONSTATUS.fields_by_name['position_variance'].message_type = _NED
-_NAVIGATIONSTATUS.fields_by_name['orientation_variance'].message_type = geometry__pb2._VECTOR3
+_NAVIGATIONSTATUS.fields_by_name['orientationRate'].message_type = geometry__pb2._VECTOR3
+_NAVIGATIONSTATUS.fields_by_name['positionVariance'].message_type = _NED
+_NAVIGATIONSTATUS.fields_by_name['orientationVariance'].message_type = geometry__pb2._VECTOR3
 DESCRIPTOR.message_types_by_name['NED'] = _NED
 DESCRIPTOR.message_types_by_name['NavigationStatus'] = _NAVIGATIONSTATUS
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
