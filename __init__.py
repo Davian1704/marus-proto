ba41c776f845365f9f09edbb291eb658206bfba3
Automation <generator@github.action>
2023-02-17 10:16:24 +0000

    Python code generated based on revision 69a3b91.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 import sys
 from os.path import dirname
-sys.path.insert(0, dirname(__file__))
+sys.path.insert(1, dirname(__file__))
--- bellhop_pb2_grpc.py
+++ bellhop_pb2_grpc.py
@@ -2,11 +2,10 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import commander_service_pb2 as commander__service__pb2
-import labust_pb2 as labust__pb2
+import bellhop_pb2 as bellhop__pb2
 
 
-class CommanderStub(object):
+class BellhopStub(object):
     """Missing associated documentation comment in .proto file."""
 
     def __init__(self, channel):
@@ -15,42 +14,42 @@ class CommanderStub(object):
         Args:
             channel: A grpc.Channel.
         """
-        self.PrimitivePointer = channel.unary_unary(
-                '/service.Commander/PrimitivePointer',
-                request_serializer=labust__pb2.PointerPrimitiveService.SerializeToString,
-                response_deserializer=commander__service__pb2.PrimitivePointerResponse.FromString,
+        self.BellhopSim = channel.unary_unary(
+                '/service.Bellhop/BellhopSim',
+                request_serializer=bellhop__pb2.BellhopMsg.SerializeToString,
+                response_deserializer=bellhop__pb2.BellhopRes.FromString,
                 )
 
 
-class CommanderServicer(object):
+class BellhopServicer(object):
     """Missing associated documentation comment in .proto file."""
 
-    def PrimitivePointer(self, request, context):
+    def BellhopSim(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
 
-def add_CommanderServicer_to_server(servicer, server):
+def add_BellhopServicer_to_server(servicer, server):
     rpc_method_handlers = {
-            'PrimitivePointer': grpc.unary_unary_rpc_method_handler(
-                    servicer.PrimitivePointer,
-                    request_deserializer=labust__pb2.PointerPrimitiveService.FromString,
-                    response_serializer=commander__service__pb2.PrimitivePointerResponse.SerializeToString,
+            'BellhopSim': grpc.unary_unary_rpc_method_handler(
+                    servicer.BellhopSim,
+                    request_deserializer=bellhop__pb2.BellhopMsg.FromString,
+                    response_serializer=bellhop__pb2.BellhopRes.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
-            'service.Commander', rpc_method_handlers)
+            'service.Bellhop', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
 
 
  # This class is part of an EXPERIMENTAL API.
-class Commander(object):
+class Bellhop(object):
     """Missing associated documentation comment in .proto file."""
 
     @staticmethod
-    def PrimitivePointer(request,
+    def BellhopSim(request,
             target,
             options=(),
             channel_credentials=None,
@@ -59,8 +58,8 @@ class Commander(object):
             wait_for_ready=None,
             timeout=None,
             metadata=None):
-        return grpc.experimental.unary_unary(request, target, '/service.Commander/PrimitivePointer',
-            labust__pb2.PointerPrimitiveService.SerializeToString,
-            commander__service__pb2.PrimitivePointerResponse.FromString,
+        return grpc.experimental.unary_unary(request, target, '/service.Bellhop/BellhopSim',
+            bellhop__pb2.BellhopMsg.SerializeToString,
+            bellhop__pb2.BellhopRes.FromString,
             options, channel_credentials,
             call_credentials, compression, wait_for_ready, timeout, metadata)
